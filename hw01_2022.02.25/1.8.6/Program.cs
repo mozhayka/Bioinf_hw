456b3076a1e2a607ebef93940817927a5d63d066
mozhayka <mozhay2000@gmail.com>
2022-02-23 12:00:40 +0300

    1.8.6

---
+++ hw01_2022.02.25/1.8.6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _1._8._6
+{
+    class Program
+    {
+        static int HammingDistance(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                throw new Exception("length");
+            int ans = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != s2[i])
+                    ans++;
+            }
+            return ans;
+        }
+
+        static int PatternMatching(string sub, string s, int d)
+        {
+            int ans = 0;
+            for (int i = 0; i <= s.Length - sub.Length; i++)
+            {
+                if (HammingDistance(sub, s.Substring(i, sub.Length)) <= d)
+                    ans++;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            using (var f = new StreamReader("../../../dataset.txt"))
+            {
+
+                string sub = f.ReadLine();
+                string s = f.ReadLine();
+                var d = Int32.Parse(f.ReadLine());
+
+                //string sub = "GAGG"; 
+                //string s = "TTTAGAGCCTTCAGAGG";
+                //var d = 2; 
+                //StreamWriter w = new StreamWriter("ans.txt");
+                Console.WriteLine(PatternMatching(sub, s, d));
+
+            }
+
+        }
+    }
+}
