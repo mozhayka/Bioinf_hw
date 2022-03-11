d3295b2cbfeb7505be7f218ca4a7ab7c0b2593b3
mozhayka <mozhay2000@gmail.com>
2022-03-11 11:35:37 +0300

    3.3.10

---
+++ hw03_2022.03.11/3.3.10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace _3._3._10
+{
+    class GraphDna
+    {
+        public static bool IsEdge(string from, string to)
+        {
+            return from.Substring(1) == to.Substring(0, to.Length - 1);
+        }
+    }
+
+    class Input
+    {
+        public readonly string[] text;
+
+        public Input(string filename = "dataset.txt")
+        {
+            filename = "../../../" + filename;
+            using (var f = new StreamReader(filename))
+            {
+                text = f.ReadLine().Split(' ');
+            }
+        }
+    }
+
+    class Program
+    {
+        static Input data;
+
+        static void Do()
+        {
+            data = new Input("dataset_664468_10.txt");
+            foreach (var from in data.text)
+            {
+                bool printed = false;
+                foreach (var to in data.text)
+                {
+                    if (GraphDna.IsEdge(from, to))
+                    {
+                        if (!printed)
+                        {
+                            printed = true;
+                            Console.Write($"{from}:");
+                        }
+                        Console.Write($" {to}");
+                    }
+                }
+                if (printed)
+                    Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Do();
+        }
+    }
+}
