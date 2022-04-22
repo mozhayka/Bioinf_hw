66b0b3a6e18f07aa30241e50d37e5704006f9973
mozhayka <mozhay2000@gmail.com>
2022-04-22 23:54:05 +0300

    7.6.8

---
+++ hw08_2022.04.22/7.6.8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _7._6._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input input = new("dataset_664552_8.txt");
+            DistanceMatrix m = new(input.matrix, input.n);
+            m.UPGMA();
+        }
+    }
+}
---
+++ hw08_2022.04.22/7.6.8/Tree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._6._8
+{
+    class Tree
+    {
+        readonly List<List<int>> edges;
+        readonly Dictionary<int, Dictionary<int, double>> distance;
+        public int N { get; set; }
+
+        public Tree(int n)
+        {
+            N = n;
+            edges = new();
+            distance = new();
+            for (int i = 0; i < n; i++)
+            {
+                edges.Add(new());
+                distance[i] = new();
+            }
+        }
+
+        public void AddEdge(int from, int to, double dist)
+        {
+            //Console.WriteLine($"Add edge {from} {to} {dist}");
+            edges[from].Add(to);
+            edges[to].Add(from);
+            distance[from][to] = dist;
+            distance[to][from] = dist;
+        }
+
+        public void Print()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            for (int from = 0; from < N; from++)
+            {
+                foreach (var to in edges[from])
+                {
+                    Console.WriteLine($"{from}->{to}:" +
+                        $"{string.Format("{0:f2}", distance[from][to])}");
+                }
+            }
+        }
+    }
+}
